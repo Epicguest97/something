35e926056b91b543d95ccec1395ba9e94dd8bd4c
“Mehul <kaushikmehul94@gmail.com>
2025-01-23 00:57:48 +0530

    secound

--- api/log-api.js
+++ api/log-api.js
@@ -1,9 +1,12 @@
 // api/log-ip.js
 export default function handler(req, res) {
+    // Random log to confirm the function is working
+    console.log("Serverless function is working!");
+  
     // Get the IP address from the request headers
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   
-    // Log the IP address (you can replace this with a database or external logging service)
+    // Log the IP address
     console.log(`Visitor IP: ${ip}`);
   
     // Respond to the client
